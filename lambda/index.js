46d490c85c8d85bf7e6cabe7d19e364c0dd6ddd3
DuncanCorp <119314372+DuncanCorp@users.noreply.github.com>
2022-12-02 11:00:11 +0300

    Update index.js

--- lambda/index.js
+++ lambda/index.js
@@ -1,19 +1,5 @@
-/**
- * Copyright 2020 Amazon.com, Inc. and its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
- *
- * Licensed under the Amazon Software License (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- * http://aws.amazon.com/asl/
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- **/
 
+//import required libraries
 const Alexa = require('ask-sdk-core');
 const util = require('./util');
 
@@ -85,7 +71,6 @@ const IntroToAlexaConversationsButtonEventHandler = {
  * @param handlerInput
  * @returns API response object
  *
- * See https://developer.amazon.com/en-US/docs/alexa/conversations/handle-api-calls.html
  */
 const GetFavoriteColorApiHandler = {
     canHandle(handlerInput) {
@@ -113,8 +98,6 @@ const GetFavoriteColorApiHandler = {
  *
  * @param handlerInput
  * @returns response
- *
- * See https://developer.amazon.com/en-US/docs/alexa/conversations/handle-api-calls.html
  */
 const FallbackIntentHandler = {
     canHandle(handlerInput) {
